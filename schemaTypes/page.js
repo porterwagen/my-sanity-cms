864d5c50ae944d4fdeb0465f5587116b824a1856
Daniel Porter <danielporter@Daniels-MacBook-Pro-2.local>
2025-05-09 10:35:10 -0600

    Add page schema for standalone pages

---
+++ schemaTypes/page.js
@@ -0,0 +1,78 @@
+export default {
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+    {
+      name: 'excerpt',
+      title: 'Excerpt',
+      description: 'A short description of the page content',
+      type: 'text',
+      rows: 3,
+    },
+    {
+      name: 'seo',
+      title: 'SEO',
+      type: 'object',
+      fields: [
+        {
+          name: 'metaTitle',
+          title: 'Meta Title',
+          type: 'string',
+        },
+        {
+          name: 'metaDescription',
+          title: 'Meta Description',
+          type: 'text',
+          rows: 2,
+        },
+        {
+          name: 'keywords',
+          title: 'Keywords',
+          type: 'string',
+        }
+      ]
+    }
+  ],
+  
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage',
+    },
+  },
+}
